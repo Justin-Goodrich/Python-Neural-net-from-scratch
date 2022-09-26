4d2b4af6b9f14c56be3b024a631232388efef812
Justin Goodrich <justingoodrich@Justins-Air.attlocal.net>
2022-09-26 12:18:31 -0400

    mnist implmentation works, added softmax and its derivative

--- Implementations/MNIST_digits/mnist.py
+++ Implementations/MNIST_digits/mnist.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("../../")
 
 from Network import Network
-from utils.activationFunctions import sigmoid, sigmoidPrime
+from utils.activationFunctions import sigmoid, sigmoidPrime, softmax, softmaxPrime
 from utils.costFunctions import squaredError, squaredErrorPrime
 from utils.TrainingExample import TrainingExample
 
@@ -32,19 +32,16 @@ class Mnist_predictor(Network):
     def fit(self, trainingData, epochs):
         for e in range(epochs):
             for i in trainingData:
+                # i = training_data[x]
                 output_vector = np.array([[1] if x is i[1] else [0] for x in range(10)])
                 input = torch.reshape(i[0],(784,1)).numpy()
                 actual = self.forwardPropagate(input)
                 costGradient = self.costDerivative(actual,output_vector)
-                print(actual.tolist())
-                print()
 
-                # print(actual-output_vector)
                 # print("epoch: {} error:{}".format(e, self.costFunction(output_vector,actual).flatten()))
                 self.backPropagate(input,costGradient)
 
 
-            
     def backPropagate(self, input, costGradient):
         outputs = []
         
@@ -52,51 +49,67 @@ class Mnist_predictor(Network):
         activations = [input]
 
         
-        for W in self.layers:
-            weightedOutput = np.dot(W,activation)
+        for W, B, A in zip(self.layers,self.bias, self.activations):
+            weightedOutput = np.dot(W,activation) + B
             # weightedOutput = np.dot(input,W) 
             outputs.append(weightedOutput)
-            activation = self.activation(weightedOutput)
+            activation = A(weightedOutput)
             activations.append(activation)
 
         
-        error = np.multiply(costGradient,self.activationDerivative(outputs[-1]))
+        # error = np.multiply(costGradient,self.activationDerivative(outputs[-1]))
+        error = np.multiply(costGradient,self.activationDerivatives[-1](outputs[-1]))
+
         nabla_w = []
         for i in range(1,len(self.layers)+1):
             w = self.layers[-i]
             delta_w = np.dot(error,activations[-i-1].transpose())
+            # print(delta_w)
             nabla_w.append(delta_w)
             self.layers[-i] = self.layers[-i] - (self.learningRate * delta_w)
+            self.bias[-i] = self.bias[-i] -  (self.learningRate * error)
             if i < len(self.layers): 
-                error = np.multiply(np.dot(w.transpose(),error), self.activationDerivative(outputs[-i-1]))
+                error = np.multiply(np.dot(w.transpose(),error), self.activationDerivatives[-i-1](outputs[-i-1]))   
+
+       
 
 
 
 
-        
-
-learningRate = 0.02
+   
+learningRate = .001
 
 MNIST = Mnist_predictor(learningRate,784,sigmoid,sigmoidPrime,squaredError,squaredErrorPrime)
 
-MNIST.addLayer(128)
-MNIST.addLayer(64)
-MNIST.addLayer(10)
+MNIST.addLayer(128,sigmoid,sigmoidPrime)
+# MNIST.addLayer(64,sigmoid,sigmoidPrime)
+MNIST.addLayer(10,softmax, softmaxPrime)
 
 test = test_data[0]
 
-prediction = MNIST.forwardPropagate(torch.reshape(test[0],(784,1)).numpy())
+# prediction = MNIST.forwardPropagate(torch.reshape(test[0],(784,1)).numpy())
+# print(sigmoid(np.dot(MNIST.layers[0],torch.reshape(test[0],(784,1)).numpy())).tolist())
+# print(sigmoid(np.dot(MNIST.layers[0],torch.flatten(test[0]).numpy())))
+
+
+# print(MNIST.forwardPropogate([[]]))
 # print(prediction)
 # print("untrained prediction: ")
 # print("number: {}\nprediction:{}".format(test[1],prediction))
 
-MNIST.fit(test_data,100)
+# MNIST.fit(test_data,1)
 
-# newprediction = MNIST.forwardPropagate(torch.reshape(test[0],(784,1)).numpy())
+
+# print(MNIST.layers[1][0][30].tolist())
+# print("\n\n")
+MNIST.fit(test_data,15)
+# print(MNIST.layers[1][0][50].tolist())
+
+# 
+newprediction = MNIST.forwardPropagate(torch.reshape(test[0],(784,1)).numpy())
 
 # print("trained prediction: ")
-# print("number: {}\nprediction:{}".format(test[1],newprediction))
+# print("number: {}\nprediction:{}".format(test[1],newprediction.tolist()))
 
-
-
+print(newprediction.tolist())
 # only outputing ones 
